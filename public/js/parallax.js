0bc6430f2591f14b8425b66ec0d32bab32522ebe
Tort9994 <108017207+Tort9994@users.noreply.github.com>
2022-08-19 18:30:33 -0500

    Arreglar efecto parallax

--- public/js/parallax.js
+++ public/js/parallax.js
@@ -1,14 +1,16 @@
 $(document).ready(function () {
   $(window).scroll(function () {
-    var windowWidth = $(window).window();
+    var windowWidth = $(window).width();
+
     if (windowWidth > 800) {
-      var scroll = $(window).scrollTop;
+      var scroll = $(window).scrollTop();
+
       $("header .textos").css({
         transform: "translate(0px," + scroll / 2 + "%)",
       });
 
-      $("header .textos").css({
-        transform: "translate(0px," + scroll / 2 + "%)",
+      $(".acerca-de article").css({
+        transform: "translate(0px, -" + scroll / 4 + "%)",
       });
     }
   });
